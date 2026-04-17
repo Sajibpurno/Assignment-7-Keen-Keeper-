139e2c9a44a53cec480f9c4be860357b7177d3fe
Sajibpurno <chasajib86@gmail.com>
2026-04-17 22:11:00 +0600

    successfully add the erroe to the page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div className="flex min-h-screen items-center justify-center">
+            <h1 className="text-center font-bold text-green-600 text-6xl">
+                404 | page not found
+            </h1>
+        </div>
+    );
+};
+
+export default Error;
